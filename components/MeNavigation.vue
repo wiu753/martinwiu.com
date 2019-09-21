0330d95cd64738800f3d56f88541d41632278053
wiu753 <wiu753@gmail.com>
2019-09-21 20:04:26 +0200

    Update navigation

--- components/MeNavigation.vue
+++ components/MeNavigation.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="py-5">
-		<nuxt-link class="mr-4 text-gray-500 font-medium" :to="{ name: 'index' }" exact>Home</nuxt-link>
-		<nuxt-link class="mr-4 text-gray-500 font-medium" :to="{ name: 'projects' }">Projects</nuxt-link>
-		<nuxt-link class="text-gray-500 font-medium" :to="{ name: 'uses' }">Uses</nuxt-link>
+		<nuxt-link class="mr-4 text-gray-600 font-medium hover:text-black" :to="{ name: 'index' }" exact>Home</nuxt-link>
+		<nuxt-link class="mr-4 text-gray-600 font-medium hover:text-black" :to="{ name: 'projects' }">Projects</nuxt-link>
+		<nuxt-link class="text-gray-600 font-medium hover:text-black" :to="{ name: 'uses' }">Uses</nuxt-link>
   </nav>
 </template>
 

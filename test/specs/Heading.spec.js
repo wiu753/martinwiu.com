0c3902b04ab120e2cfb0b9e44bf1cc9843a79d53
wiu753 <wiu753@gmail.com>
2019-10-20 23:13:08 +0200

    Add LinedIn svg

--- test/specs/Heading.spec.js
+++
@@ -1,8 +0,0 @@
-import { mount } from '@vue/test-utils'
-import test from 'ava'
-import MeHeading from '../../components/MeHeading'
-
-test('is a Vue instance', (t) => {
-  const wrapper = mount(MeHeading)
-  t.is(wrapper.isVueInstance(), true)
-})

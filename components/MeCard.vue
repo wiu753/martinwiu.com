e305a2f62284de410b0cd94478be5fc93e91df19
Martin Wiulsrød <wiu753@gmail.com>
2019-11-09 18:43:57 +0100

    Remove card component

--- components/MeCard.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div
-    class="rounded-lg bg-white p-2 border border-gray-300
-    md:p-4 lg:p-6 me-transition hover:bg-gray-300 active:bg-gray-400"
-  >
-    <slot></slot>
-  </div>
-</template>
--- components/MeUses.vue
+++ components/MeUses.vue
@@ -1,21 +1,14 @@
 <template>
 	<a class="hover:bg-orange-500" :href="link">
-		<me-card>
-			<div class="flex items-center">
-				<img class="w-8 mr-4 lg:w-12 lg:mr-6" :src="icon" alt="">
-				<p class="">{{ title }}</p>
-			</div>
-		</me-card>
+		<div class="rounded-lg bg-white p-2 border border-gray-300 md:p-4 lg:p-6 me-transition hover:bg-gray-300 active:bg-gray-400 flex items-center">
+			<img class="w-8 mr-4 lg:w-12 lg:mr-6" :src="icon" alt="">
+			<p class="">{{ title }}</p>
+		</div>
 	</a>
 </template>
 
 <script>
-import MeCard from '@/components/MeCard'
-
 export default {
-	components: {
-		MeCard
-	},
 	props: {
 		title: String,
 		link: String,

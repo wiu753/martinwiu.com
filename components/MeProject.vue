20d293d599cbb83623ffc0e9762df75b73fc4812
Martin Wiulsrød <wiu753@gmail.com>
2019-12-19 18:40:15 +0100

    Update projects page

--- components/MeProject.vue
+++ components/MeProject.vue
@@ -1,11 +1,17 @@
 <template>
 	<div class="mb-2 md:text-xl lg:mb-3">
 		<a :href="link">
-			<div class="rounded-lg bg-white border border-gray-300 p-2 me-transition hover:bg-gray-300 active:bg-gray-400 md:p-4">
+			<div
+				class="rounded-lg p-2 me-transition md:p-4"
+				:class="!underDevelopment ? projectCard : projectCardDev"
+			>
 				<h1 class="text-xl mb-2"><slot name="title"></slot></h1>
-				<p class="text-gray-700"><slot name="description"></slot></p>
+				<p class="description hover:text-black" :class="!underDevelopment ? description : descriptionDev"><slot name="description"></slot></p>
 				<div class="flex text-sm flex-wrap mt-3">
-					<span v-for="(badge, index) in badges" :key="index"  class=" bg-gray-200 border border-gray-300 rounded-full px-2 mr-1 mt-1">
+					<span v-if="underDevelopment" class="badge bg-red-200 border border-gray-300 rounded-full px-2 mr-1 mt-1 text-black">
+						Under Development
+					</span>
+					<span v-for="(badge, index) in badges" :key="index"  class="badge bg-gray-200 border border-gray-300 rounded-full px-2 mr-1 mt-1">
 						{{ badge }}
 					</span>
 				</div>
@@ -18,7 +24,39 @@
 export default {
 	props: {
 		link: { type: String, required: true },
-		badges: { type: Array, required: true }
-	}
+		badges: { type: Array, required: true },
+		underDevelopment: { type: Boolean, default: false }
+	},
+
+	data: () => ({
+		projectCard: [
+			'project-card',
+			'bg-white',
+			'hover:bg-gray-300',
+			'active:bg-gray-400',
+			'border',
+			'border-grey-300'
+		],
+		projectCardDev: [
+			'bg-gray-200',
+			'text-gray-500'
+		],
+		description: [
+			'text-gray-700'
+		],
+		descriptionDev: [
+			'text-gray-500'
+		]
+	})
 }
 </script>
+
+<style scoped>
+.project-card:hover .description{
+	color: black;
+}
+
+.project-card:hover .badge {
+	background-color: white;
+}
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,7 +1,15 @@
 <template>
 	<div class="px-8 md:px-32 xl:pl-64 md:max-w-3xl xl:max-w-5xl">
 			<h1 class="text-3xl mb-4 font-semibold">Projects</h1>
-      <me-project class="mb-4 md:text-xl lg:mb-5" link="https://github.com/wiu753/martinwiulsrod.me" :badges="['Nuxt.js', 'Tailwindcss']">
+      <me-project link="https://www.typingmonster.com/" :badges="['Vue.js', 'Tailwindcss', 'Laravel', 'MySql']" underDevelopment>
+        <template v-slot:title>
+          Typing Monster
+        </template>
+        <template v-slot:description>
+          Full-stack website for doing typing tests.
+        </template>
+      </me-project>
+      <me-project link="https://github.com/wiu753/martinwiulsrod.me" :badges="['Nuxt.js', 'Tailwindcss']">
         <template v-slot:title>This website</template>
         <template v-slot:description>
           Read about me and what I do. Used for testing new technologies.

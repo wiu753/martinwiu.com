06dcbcf3abc34a58a29ea81864491437b9fa99c8
Martin Wiulsrød <wiu753@gmail.com>
2020-01-29 19:36:38 +0100

    Update knowledge section and change hover effects,

--- components/MeProject.vue
+++ components/MeProject.vue
@@ -1,62 +1,57 @@
 <template>
-	<div class="mb-2 md:text-xl lg:mb-3">
-		<a :href="link">
-			<div
-				class="rounded-lg p-2 me-transition md:p-4"
-				:class="!underDevelopment ? projectCard : projectCardDev"
-			>
-				<h1 class="text-xl xl:text-2xl mb-2"><slot name="title"></slot></h1>
-				<p class="description hover:text-black" :class="!underDevelopment ? description : descriptionDev"><slot name="description"></slot></p>
-				<div class="flex text-sm flex-wrap mt-3">
-					<span v-if="underDevelopment" class="badge bg-red-200 border border-gray-300 rounded-full px-2 mr-1 mt-1 text-black">
+    <div class="mb-2 md:text-xl lg:mb-3">
+        <a :href="link">
+            <div
+                    class="rounded-lg p-2 me-transition md:p-4"
+                    :class="!underDevelopment ? projectCard : projectCardDev"
+            >
+                <h1 class="text-xl xl:text-2xl mb-2">
+                    <slot name="title"></slot>
+                </h1>
+                <p class="description" :class="!underDevelopment ? description : descriptionDev">
+                    <slot name="description"></slot>
+                </p>
+                <div class="flex text-sm flex-wrap mt-3">
+					<span v-if="underDevelopment"
+                          class="badge bg-red-200 border border-gray-300 rounded-full px-2 mr-1 mt-1 text-black">
 						Under Development
 					</span>
-					<span v-for="(badge, index) in badges" :key="index"  class="badge bg-gray-200 border border-gray-300 rounded-full px-2 mr-1 mt-1">
+                    <span v-for="(badge, index) in badges" :key="index"
+                          class="badge bg-gray-200 border border-gray-300 rounded-full px-2 mr-1 mt-1">
 						{{ badge }}
 					</span>
-				</div>
-			</div>
-		</a>
-	</div>
+                </div>
+            </div>
+        </a>
+    </div>
 </template>
 
 <script>
-export default {
-	props: {
-		link: { type: String, required: true },
-		badges: { type: Array, required: true },
-		underDevelopment: { type: Boolean, default: false }
-	},
+    export default {
+        props: {
+            link: {type: String, required: true},
+            badges: {type: Array, required: true},
+            underDevelopment: {type: Boolean, default: false}
+        },
 
-	data: () => ({
-		projectCard: [
-			'project-card',
-			'bg-white',
-			'hover:bg-gray-300',
-			'active:bg-gray-400',
-			'border',
-			'border-grey-300'
-		],
-		projectCardDev: [
-			'bg-gray-200',
-			'text-gray-500'
-		],
-		description: [
-			'text-gray-700'
-		],
-		descriptionDev: [
-			'text-gray-500'
-		]
-	})
-}
+        data: () => ({
+            projectCard: [
+                'project-card',
+                'bg-white',
+                'hover:shadow-lg',
+                'border',
+                'border-grey-300'
+            ],
+            projectCardDev: [
+                'bg-gray-200',
+                'text-gray-500'
+            ],
+            description: [
+                'text-gray-700'
+            ],
+            descriptionDev: [
+                'text-gray-500'
+            ]
+        })
+    }
 </script>
-
-<style scoped>
-.project-card:hover .description{
-	color: black;
-}
-
-.project-card:hover .badge {
-	background-color: white;
-}
-</style>

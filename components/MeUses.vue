183347fc0953c6e7331892dc557daa391b6d5d72
Martin Wiulsrød <wiu753@gmail.com>
2020-02-08 08:54:58 +0100

    Add a reusable knowlege component

--- components/MeUses.vue
+++ components/MeUses.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="mb-2 md:text-xl lg:mb-3">
 		<a :href="link">
-			<div class="rounded-lg bg-white p-2 border border-gray-300 md:p-4 lg:p-6 me-transition hover:bg-gray-300 active:bg-gray-400 flex items-center">
+			<div class="rounded-lg bg-white p-2 border border-gray-300 md:p-4 lg:p-6 me-transition hover:shadow-lg flex items-center">
 				<slot name="icon"></slot>
 				<slot name="title"></slot>
 			</div>

73331da48e949ea5483b5d91c97fab77bd876a2c
Martin Wiulsrød <wiu753@gmail.com>
2020-05-26 19:23:32 +0200

    Remove old project

--- components/MeHeading.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="flex pt-6 pl-8 md:px-32 md:pt-16 me-gradient text-white">
-    <nuxt-link :to="{ name: 'index' }" class="flex items-center">
-      <img class="w-12 h-12 rounded-full mr-4 lg:w-20 lg:h-20" src="@/assets/img/me.jpg" alt="Profile picture of Martin Wiulsrød">
-      <h1 class="text-2xl font-medium placeholder md:text-3xl lg:pl-8">Martin Wiulsrød</h1>
-    </nuxt-link>
-  </div>
-</template>
--- components/MeWave.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <svg class="fill-current text-gray-100 me-gradient" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 200">
-    <path d="M0,160L120,144C240,128,480,96,720,85.3C960,75,1200,85,1320,90.7L1440,96L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z">
-    </path>
-  </svg>
-</template>

978c4d009f7685d7252c6770f29513f6b46e01e9
Martin Wiulsrød <wiu753@gmail.com>
2020-06-08 11:22:40 +0200

    Add blog post template

---
+++ components/BlogPost.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="max-w-4xl px-4 mt-12 sm:px-0 sm:mx-16 lg:mx-auto">
+    <header>
+      <h1><slot name="title"></slot></h1>
+      <div class="text-sm text-gray-400">
+        <slot name="info"></slot>
+      </div>
+    </header>
+    <main>
+      <slot name="body"></slot>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  @apply text-2xl;
+}
+p {
+  @apply mt-6;
+}
+</style>

f353bdf679840a4b0723a63c8c78d146819359ca
Martin Wiulsrød <wiu753@gmail.com>
2020-06-08 12:19:12 +0200

    Update tailwind purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-  purge: ['./components/*.vue', './pages/*.vue'],
+  purge: ['./components/*.vue', './pages/*.vue', './pages/**/*.vue'],
   theme: {},
   variants: {},
   plugins: [require('@tailwindcss/ui')]

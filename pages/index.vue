f5d43f52948bb1d744c4da8b9f1e03d30e33c474
Martin Wiu <wiu753@gmail.com>
2021-12-18 11:13:28 +0100

    Update about me page

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,25 @@
 <template>
   <main>
     <p class="mb-6">
-      Hello! My name is Martin Wiu. I'm a {{ age }}-year-old Norwegian web
-      developer currently studying software engineering at the University of
-      South-Eastern Norway.
+      Hello! My name is Martin Wiu. I'm a 26-year-old Norwegian web developer
+      currently studying software engineering at the University of South-Eastern
+      Norway.
     </p>
     <p class="mb-6">
       I'm a minimalist, lifelong learner, and dog lover. People usually describe
-      me as a pretty calm, collected, and accepting guy. In my free time, I love
-      to read books hang out with friends and play golf. Some of my favorite
-      topics when reading is mental health, technology, and finance.
+      me as a pretty calm, collected, and accepting guy. I love to read books,
+      hang out with friends, and play golf in my free time. When reading, my
+      favorite topics are mental health, technology, and finance.
     </p>
     <p>
       Occasionally I
       <nuxt-link class="underline" to="/posts">write</nuxt-link> about the
-      subjects that I fnid the most interesting. If you want to contact me you
-      can send me an email at
+      subjects that I find the most interesting. If you want to contact me, you
+      can email me at
       <a class="underline" href="mailto:wiu753@gmail.com">wiu753@gmail.com</a>.
       I'm also on
       <a href="https://www.twitter.com/wiu753" class="underline">Twitter</a>,
-      <a href="https://www.github.com/wiu753" class="underline">GitHub</a>
-      and
+      <a href="https://www.github.com/wiu753" class="underline">GitHub</a>, and
       <a
         href="https://www.linkedin.com/in/martin-wiulsr%C3%B8d-3a1412161/"
         class="underline"

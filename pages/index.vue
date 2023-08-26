c4c0b48edfc297088089e29a750406ccb305875f
Martin Wiulsrød <wiu753@gmail.com>
2023-08-26 17:48:15 +0200

    Update text

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <template>
   <main>
     <p class="mb-6">
-      Hello! My name is Martin Wiu. I'm a {{ age }}-year-old Norwegian software
-      developer currently studying for a bachelor's in computer engineering with
-      a specialty in cybersecurity at the University of South-Eastern Norway. I
-      also have a bachelor's in information systems and two years of working
-      experience as a software developer.
+      Hello! My name is Martin Wiu. I'm a {{ age }}-year-old Norwegian software engineer with Bachelor of Science
+      degrees in Information Systems and IT Leadership as well as Computer Engineering. I am currently working
+      as a software engineer at Kongsberg Norcontrol.
     </p>
     <p class="mb-6">
       I'm a lifelong learner and minimalist. I love to read books, play games
@@ -17,9 +15,7 @@
         <a class="underline" href="mailto:wiu753@gmail.com">Email</a>,
         <a href="https://www.twitter.com/wiu753" class="underline">Twitter</a>,
         <a href="https://www.github.com/wiu753" class="underline">GitHub</a> and
-        <a href="https://www.linkedin.com/in/martinwiu" class="underline"
-          >LinkedIn</a
-        >
+        <a href="https://www.linkedin.com/in/martinwiu" class="underline">LinkedIn</a>
       </span>
     </p>
   </main>

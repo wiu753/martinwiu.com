a41aed23115eb868b25746c5c199ea80581be0af
Martin Wiu <martinwiu@proton.me>
2024-09-21 13:08:46 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,14 +1,3 @@
-<script setup lang="ts">
-const dateString = '1995/06/21'
-const today = new Date()
-const birthDate = new Date(dateString)
-let age = today.getFullYear() - birthDate.getFullYear()
-const m = today.getMonth() - birthDate.getMonth()
-if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-  age--
-}
-</script>
-
 <template>
   <NuxtRouteAnnouncer />
   <div class="flex flex-col max-w-3xl min-h-screen mx-4 sm:mx-16 lg:text-2xl md:text-xl">
@@ -16,17 +5,13 @@ if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
     <div class="flex-1 mt-12">
       <main>
         <p class="mb-6">
-          Hello! My name is Martin Wiu. I'm a {{ age }}-year-old Norwegian software engineer with two Bachelor of Science degrees: one in Information Systems and IT Leadership, and the other in Computer Engineering. I am currently working
-          as a software engineer in Kongsberg Gruppen.
+          Hello!
         </p>
-        <p class="mb-6">
-          I'm a lifelong learner and minimalist. I love to read books and play golf. When reading, my favorite topics besides tech
-          are psychology and philosophy.
-        </p>
-        <p class="flex flex-col">
+        <p>I am Martin Wiu — a software engineer, reader, golfer, and minimalist.</p>
+        <p class="mt-6 flex flex-col">
           <span>
-            <a class="underline" href="mailto:martinwiu@proton.me">Email</a>,
-            <a href="https://www.github.com/wiu753" class="underline">GitHub</a> and
+            <a class="underline" href="mailto:martinwiu@proton.me">Email</a> and
+            <a href="https://www.github.com/wiu753" class="underline">GitHub</a>.
           </span>
         </p>
       </main>
